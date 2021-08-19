69ea5c5b949a3ce3beda5d8e8793cf2f48b2e9bb
Sigit Purwadi HW <sgt2426@gmail.com>
2021-08-19 21:58:40 +0700

    More improvement update

--- sgitsp.js
+++ sgitsp.js
@@ -16,10 +16,25 @@ const twitterClient = new TwitterClient({
   accessTokenSecret: process.env.CONSUMER_SECRET,
 });
 
-// Function to crop circle image
-const width = 96,
-    r = width / 2,
-    circleShape = Buffer.from(`<svg><circle cx="${r}" cy="${r}" r="${r}" /></svg>`);
+// Curent Date
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var today = new Date();
+/*let time = today.getHours() + ":" + today.getMinutes()/* + ":" + today.getSeconds()*/;
+let day = days[today.getDay()];
+let month = months[today.getMonth()]
+let date = today.getDate() + " " + month + " " + today.getFullYear();
+
+function digiTime(date) {
+  var hours = ("0" + today.getHours()).slice(-2);
+  var minutes = ("0" + today.getMinutes()).slice(-2);
+  var ampm = hours >= 12 ? 'PM' : 'AM';
+  /*hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;*/
+  let time = hours + ':' + minutes/* + ' ' + ampm*/;
+  return time;
+}
 
 // Function to get latest folowers
 async function getLatestFollowers() {
@@ -27,7 +42,9 @@ async function getLatestFollowers() {
     screen_name: process.env.TWITTER_HANDLE,
     count: 3,
   });
-// Download avatar...
+  console.log(`Fetch latest followers...`);
+
+// Download avatar image...
   let count = 0;
   const downloads = new Promise((resolve, reject) => {
     data.users.forEach((user, index, arr) => {
@@ -43,7 +60,7 @@ async function getLatestFollowers() {
   });
 }
 
-// Resize downloaded images
+// Function to resize downloaded avatar
 async function downloadImage(url, image_path) {
   await axios({
     url,
@@ -60,8 +77,15 @@ async function downloadImage(url, image_path) {
           .toFile(image_path));
       })
   );
+  console.log(`Crop and rezise avatar images...`);
 }
 
+// Function to crop image
+const width = 96,
+    r = width / 2,
+    circleShape = Buffer.from(`<svg><circle cx="${r}" cy="${r}" r="${r}" /></svg>`);
+
+// Function to get latest article via feed.xml
 /*
 async function getLatestArticleHeadline() {
   let title = '';
@@ -74,21 +98,33 @@ async function getLatestArticleHeadline() {
 }
 */
 
+// Additional credit text
+let credit = 'This header image has been generated using special code by @sgitsp';
+
 // Function to draw image
 async function drawBanner() {
   const images = ['1500x500-default.png', '0.png', '1.png', '2.png'];
   const promiseArray = [];
+  const dayFont = await Jimp.loadFont('fonts/TimeandSpace-Regular.ttf.fnt');
+  const timeFont = await Jimp.loadFont("fonts/Caviar_Dreams_Bold_64.fnt");
+  const dateFont = await Jimp.loadFont(Jimp.FONT_SANS_16_WHITE)
   images.forEach((image) => promiseArray.push(Jimp.read(image)));
   //promiseArray.push(getLatestArticleHeadline());
-  //promiseArray.push(Jimp.loadFont(Jimp.FONT_SANS_32_BLACK));
+  promiseArray.push(Jimp.loadFont(Jimp.FONT_SANS_16_WHITE));
 
   Promise.all(promiseArray).then(
-    ([banner, imageOne, imageTwo, imageThree, headline, font]) => {
-      console.log(`Current headline: ${headline}`);
+    ([banner, imageOne, imageTwo, imageThree/*, headline*/, font]) => {
       banner.composite(imageOne, 1082, 45);
       banner.composite(imageTwo, 1193, 45);
       banner.composite(imageThree, 1304, 45);
-      //banner.print(font, 410, 410, headline);
+      console.log(`3 latest followers added`);
+      banner.print(dayFont, 585, 110, day);
+      banner.print(timeFont, 380, 92, digiTime(new Date));
+      banner.print(dateFont, 584, 132, date);
+      console.log(`Additional cosmetic added`);
+      banner.print(font, 500, 465, credit);
+      console.log(`Generating new header...`);
+      console.log(`Last sync: ${day}, ${date} (${digiTime(new Date)})`);
       banner.write('1500x500-draw.png', function () {
         uploadBanner();
       });
@@ -104,7 +140,7 @@ async function uploadBanner() {
       banner: base64,
     })
     .then((d) => {
-      console.log('Upload to Twitter done');
+      console.log('Upload to Twitter done!');
     });
 }
 
